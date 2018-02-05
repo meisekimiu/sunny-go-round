25d5fe2c7c63a6a7cad80838bcbb2399094d243a
derP@765pro <Giratina493@mew151.net>
2018-02-05 09:51:39 -0700

    Transition animation code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import TestSlideA from './slides/TestSlide1';
 import TestSlideB from './slides/TestSlide2';
 
+import {Modulo} from './Helpers.js';
+
+import './animation.css';
+
 class SunnyGoRound extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			currentSlide: 0,
-			transitionSlide: 0
+			transitionSlide: 0,
+			delta: 0
 		};
 		this.slides = [
 			<TestSlideA />,
@@ -16,26 +21,61 @@ class SunnyGoRound extends Component {
 	}
 	getSlides() {
 		if(this.state.currentSlide === this.state.transitionSlide) {
-			return this.slides[this.state.currentSlide];
+			return (
+				<div style={{position: "relative"}}>
+					<div className="animatedTransition">
+						{this.slides[this.state.currentSlide]}
+					</div>
+				</div>
+			);
 		}
 		else {
-			return "Transitioning!"
+			return (
+				<div style={{position: "relative"}}>
+					<div style={{animationName: this.getExitTransitionName()}} className="animatedTransition">
+						{this.slides[this.state.currentSlide]}
+					</div>
+					<div style={{animationName: this.getEnterTransitionName()}} className="animatedTransition">
+						{this.slides[this.state.transitionSlide]}
+					</div>
+				</div>
+			);
 		}
 	}
 	moveSlide(position) {
-		let newslide = (this.state.currentSlide+position) % this.slides.length;
-		let movement = newslide != this.state.currentSlide;
+		let newslide = Modulo(this.state.currentSlide+position,this.slides.length);
+		let movement = newslide !== this.state.currentSlide;
 		this.setState({
-			transitionSlide: newslide
+			transitionSlide: newslide,
+			delta: position
 		});
 		if(movement) {
-			setTimeout(() => {this.completeTransition()},600);
+			setTimeout(() => {this.completeTransition()},1000);
 		}
 	}
 	completeTransition() {
 		this.setState({
 			currentSlide: this.state.transitionSlide
 		});
+	}
+	transitionIsFromLeft() {
+		return this.state.delta > 0;
+	}
+	getEnterTransitionName() {
+		if(this.transitionIsFromLeft()) {
+			return "enter-to-left";
+		}
+		else {
+			return "enter-to-right";
+		}
+	}
+	getExitTransitionName() {
+		if(this.transitionIsFromLeft()) {
+			return "exit-to-left";
+		}
+		else {
+			return "exit-to-right";
+		}
 	}
 	render() {
 		return (
