93733f97c8fc040957e260e0f3748decc2108642
derP@765pro <Giratina493@mew151.net>
2018-02-05 08:42:49 -0700

    New test slide

---
+++ src/slides/TestSlide2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TestSlideB extends Component {
+	render() {
+		return (
+			<div className="slide">
+				hai hai
+			</div>
+		);
+	}
+}
+
+export default TestSlideB;
