3952f0196cff6e1df8aecaeb9ee209d85b91e936
derP@765pro <Giratina493@mew151.net>
2018-02-12 16:26:37 -0700

    oops

--- src/slides/SunnyUtils.js
+++ src/slides/SunnyUtils.js
@@ -1,7 +1,7 @@
 const SunnyUtils = {
 	getPath: function(path) {
 		if(window.sunnyUtils) {
-			return window.sunnyUtils.getpath(path);
+			return window.sunnyUtils.getPath(path);
 			/*This lets us get build info from the main Taiyou website*/
 		}
 		else {
